3dc7a1c18b4a892161c0311564e9d037940d58c5
Mohibbull Mohib <33421746+Mohib04@users.noreply.github.com>
2021-04-18 19:46:49 +0600

    added

--- mohibbullamunshi2191081136.php
+++ mohibbullamunshi2191081136.php
@@ -1,4 +1,27 @@
 <?php
-echo "Hello World";
+//Variables
+$text_variable = "My Name Is Mohibbulla Munshi and My ID is: 2191081136";
+echo $text_variable;
+echo "\n \n";
+
+//Operators
+$var1 = "100";
+$var2 = 100;
+if($var1 === $var2){
+    echo "Identical";
+}else{
+    echo "Not Identical";
+}
+echo "\n \n";
+if($var1 == $var2){
+    echo "Identical";
+}else{
+    echo "Not Identical";
+}
+echo "\n \n";
+//Constant
+define("university", "Uttara University");
+echo university;
+
 
 ?>
